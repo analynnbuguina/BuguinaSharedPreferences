d8a1040509cacbc2b435fcf2b39dbcc1eed2fe51
ABuguina <analynnj.buguina.iics@ust.edu.ph>
2017-10-24 19:27:02 +0800

    commit

--- app/src/main/java/com/example/abuguina/buguinasharedpreferences/MainActivity.java
+++ app/src/main/java/com/example/abuguina/buguinasharedpreferences/MainActivity.java
@@ -4,43 +4,49 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
-    EditText etUsername, etPassword;
-    Button btnSave, btnDisplay;
-    TextView tvDisplay;
+public class MainActivity extends AppCompatActivity {
+    EditText et_username;
+    EditText et_password;
+    Button btn_save;
+    Button btn_display;
+    TextView tv_display;
     SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etUsername = (EditText) findViewById(R.id.et_username);
-        etPassword = (EditText) findViewById(R.id.et_password);
-        btnSave = (Button) findViewById(R.id.btn_save);
-        btnDisplay = (Button) findViewById(R.id.btn_display);
-        tvDisplay = (TextView) findViewById(R.id.tv_display);
-        preferences = getPreferences(Context.MODE_PRIVATE);
+        et_username = (EditText) findViewById(R.id.eTusername);
+        et_password = (EditText) findViewById(R.id.eTpassword);
+        btn_save = (Button) findViewById(R.id.saveBtn);
+        btn_display = (Button) findViewById(R.id.displayBtn);
+        tv_display = (TextView) findViewById(R.id.tVdisplay);
+        preferences = (getPreferences(Context.MODE_PRIVATE));
 
     }
 
-    public void saveInfo(View view) {
+    public void saveInfo(View view){
         SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("username", etUsername.getText().toString());
-        editor.putString("password", etPassword.getText().toString());
+        editor.putString("username", et_username.getText().toString());
+        editor.putString("password", et_password.getText().toString());
         editor.commit();
         Toast.makeText(this, "Data Saved!", Toast.LENGTH_SHORT).show();
+
     }
 
-    public void loadInfo(View view) {
-        String user = preferences.getString("username", "");
-        String pwd = preferences.getString("password", "");
-        tvDisplay.setText("The password of" + user + "is" + pwd);
+    public void loadInfo(View view){
+        String user = preferences.getString("username","");
+        String pwd = preferences.getString("password","");
+        tv_display.setText("The password of " + user + " is " + pwd);
     }
 }
